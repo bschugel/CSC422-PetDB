1df8c80ba184358a8b060c9b33f67c2b8dd80f51
Ben <Ben@Ben-PC>
2020-05-14 21:19:12 -0500

    Create pets and list pets functional
    TODO make pet input only 1 line

--- src/petSearch/PetSearch.java
+++ src/petSearch/PetSearch.java
@@ -6,11 +6,13 @@ public class PetSearch {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		boolean loop = true;
-		Pet[] pets = new Pet[100];
+		boolean loop2 = true;
+		Pet pets[] = new Pet[100];
 		int petCount = 0;
-		
-		System.out.print("Pet Database Program.\r\n\n" + 	
-				"What would you like to do?\r\n" + 
+		int input=0;
+		System.out.println("Pet Database Program.\r\n");
+		while(loop) {
+		System.out.print("What would you like to do?\r\n" + 
 				" 1) View all pets\r\n" + 
 				" 2) Add more pets\r\n" + 
 				" 3) Update an existing pet\r\n" + 
@@ -19,56 +21,62 @@ public class PetSearch {
 				" 6) Search pets by age\r\n" + 
 				" 7) Exit program\r\n" + 
 				"Your choice: ");
-		int input=0;
-		while(loop) {					//Continuous loop until valid input entered (No error checking on input type at the moment)
+
+			while(loop2) {					//Continuous loop until valid input entered (No error checking on input type at the moment)
+				
+				input = sc.nextInt();              
+				if(input>0&&input<8) {
+					System.out.println("ok");
+					loop2=false;
+				}
+				else {
+					System.out.println("Please enter a value from 1 to 7\r\n"
+							+ "Your choice: ");
+				}
+			}
+			loop2=true;
 			
-			input = sc.nextInt();              
-			if(input>0&&input<8) {
-				System.out.println("ok");
-				loop=false;
-			}
-			else {
-				System.out.println("Please enter a value from 1 to 7\r\n"
-						+ "Your choice: ");
+			switch(input) {			//Menu input selection switch
+			case 1:
+				System.out.println("View all pets here");
+				for(int i=0; i<petCount; i++) {
+					System.out.println("Name: "+pets[i].getName()+"  Age: "+pets[i].getAge());
+				}
+				break;
+			case 2:
+				if(sc.hasNextLine()) {
+					sc.nextLine();
+				}
+				System.out.println("Add pet here");
+				
+				System.out.println("Input name");
+				String nameInput = sc.nextLine();
+				System.out.println("Input age");
+				int ageInput = sc.nextInt();
+				pets[petCount]= new Pet();
+				pets[petCount].setName(nameInput);
+				pets[petCount].setAge(ageInput);
+				System.out.println("Name: "+pets[petCount].getName()+"  Age: "+pets[petCount].getAge());
+				petCount++;
+				break;
+			case 3:
+				System.out.println("Update pet TODO");	//Release 3
+				break;
+			case 4:
+				System.out.println("Remove pet TODO");	//Release 3
+				break;
+			case 5:
+				System.out.println("Search by Name TODO");	//Release 2
+				break;
+			case 6:
+				System.out.println("Search by Age TODO");	//Release 2
+				break;
+			case 7:
+				System.exit(0);
+				break;
 			}
 		}
-		switch(input) {			//Menu input selection switch
-		case 1:
-			System.out.println("View all pets here");
-			for(int i=0; i<petCount; i++) {
-				System.out.println("Name"+pets[i].getName());
-			}
-			break;
-		case 2:
-			if(sc.hasNextLine()) {
-				sc.nextLine();
-			}
-			System.out.println("Add pet here");
-			System.out.println("Input name");
-			String nameInput = sc.nextLine();
-			System.out.println("Input age");
-			int ageInput = sc.nextInt();
-			System.out.println(nameInput);
-			pets[petCount].setName(nameInput);
-			pets[petCount].setAge(ageInput);
-			petCount++;
-			break;
-		case 3:
-			System.out.println("Update pet TODO");	//Release 3
-			break;
-		case 4:
-			System.out.println("Remove pet TODO");	//Release 3
-			break;
-		case 5:
-			System.out.println("Search by Name TODO");	//Release 2
-			break;
-		case 6:
-			System.out.println("Search by Age TODO");	//Release 2
-			break;
-		case 7:
-			System.exit(0);
-			break;
-		}
+		
 		
 	}
 }
